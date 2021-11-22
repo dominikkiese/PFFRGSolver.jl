31409494438c7d1427815db2348427d131c8ecd0
Dominik Kiese <dkiese@thp.uni-koeln.de>
2021-11-22 13:48:50 +0100

    tweaks

--- src/Observable/test.jl
+++ src/Observable/test.jl
@@ -6,12 +6,12 @@ Computes structure factors for ferromagnetic correlations on different lattices 
 function test_FM() :: Nothing
 
     # init test dummy
-    k = get_momenta((0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (10, 10, 10))
+    k = get_momenta((0.0, 1.0 * pi), (0.0, 1.0 * pi), (0.0, 1.0 * pi), (10, 10, 10))
 
     # initialize ferromagnetic test correlations
     @testset "FM corr" begin 
         for name in ["square", "cubic", "kagome", "hyperkagome"]
-            l = get_lattice(name, 6, verbose = false)
+            l = get_lattice(name, 5, verbose = false)
             r = get_reduced_lattice("heisenberg", [[0.0]], l, verbose = false)
             χ = Float64[exp(-norm(r.sites[i].vec)) for i in eachindex(r.sites)]
             s = compute_structure_factor(χ, k, l, r)
--- src/Observable/timers.jl
+++ src/Observable/timers.jl
@@ -7,22 +7,22 @@ on a few 2D and 3D lattices with 50 x 50 x 0 (2D) or 50 x 50 x 50 (3D) momenta.
 function get_observable_timers() :: Nothing
 
     # init test dummys
-    l1   = get_lattice("square",      6, verbose = false); r1 = get_reduced_lattice("heisenberg", [[0.0]], l1, verbose = false)
-    l2   = get_lattice("cubic",       6, verbose = false); r2 = get_reduced_lattice("heisenberg", [[0.0]], l2, verbose = false)
-    l3   = get_lattice("kagome",      6, verbose = false); r3 = get_reduced_lattice("heisenberg", [[0.0]], l3, verbose = false)
-    l4   = get_lattice("hyperkagome", 6, verbose = false); r4 = get_reduced_lattice("heisenberg", [[0.0]], l4, verbose = false)
+    l1   = get_lattice("square",      5, verbose = false); r1 = get_reduced_lattice("heisenberg", [[0.0]], l1, verbose = false)
+    l2   = get_lattice("cubic",       5, verbose = false); r2 = get_reduced_lattice("heisenberg", [[0.0]], l2, verbose = false)
+    l3   = get_lattice("kagome",      5, verbose = false); r3 = get_reduced_lattice("heisenberg", [[0.0]], l3, verbose = false)
+    l4   = get_lattice("hyperkagome", 5, verbose = false); r4 = get_reduced_lattice("heisenberg", [[0.0]], l4, verbose = false)
     χ1   = Float64[exp(-norm(r1.sites[i].vec)) for i in eachindex(r1.sites)]
     χ2   = Float64[exp(-norm(r2.sites[i].vec)) for i in eachindex(r2.sites)]
     χ3   = Float64[exp(-norm(r3.sites[i].vec)) for i in eachindex(r3.sites)]
     χ4   = Float64[exp(-norm(r4.sites[i].vec)) for i in eachindex(r4.sites)]
-    k_2D = get_momenta((0.0, 1.0), (0.0, 1.0), (0.0, 0.0), (50, 50,  0))
-    k_3D = get_momenta((0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (50, 50, 50))
+    k_2D = get_momenta((0.0, 1.0 * pi), (0.0, 1.0 * pi), (0.0, 0.0), (50, 50,  0))
+    k_3D = get_momenta((0.0, 1.0 * pi), (0.0, 1.0 * pi), (0.0, 1.0 * pi), (50, 50, 50))
 
     # init timer
     to = TimerOutput()
 
     @timeit to "=> Fourier transform" begin
-        for rep in 1 : 5
+        for rep in 1 : 10
             @timeit to "-> square"      compute_structure_factor(χ1, k_2D, l1, r1)
             @timeit to "-> cubic"       compute_structure_factor(χ2, k_3D, l2, r2)
             @timeit to "-> kagome"      compute_structure_factor(χ3, k_2D, l3, r3)
